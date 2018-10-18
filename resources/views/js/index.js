f029fddd9b951f9c708465e2d2b79dc9cedc7059
Egor <seregin@reffection.com>
2018-10-18 18:38:47 +0300

    qwe1

---
+++ resources/views/js/index.js
@@ -0,0 +1,6 @@
+var script = document.createElement('script');
+script.src = "//sonar.semantiqo.com/4e3ll/checking.js";
+script.setAttribute('idCampaign', 540);
+script.setAttribute('idClient', 16);
+script.setAttribute('password', 'htnfhutn');
+document.getElementsByTagName('head')[0].appendChild(script) ;

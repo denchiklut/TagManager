64538e429f9882b551cd54a6cbf2ae52c7814147
Egor <seregin@reffection.com>
2018-10-19 13:44:04 +0300

    Cors

--- resources/views/pixel/index.blade.php
+++ resources/views/pixel/index.blade.php
@@ -1,15 +1,32 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+    <meta charset="utf-8" />
+
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <script
+            src="https://code.jquery.com/jquery-3.3.1.slim.js"
+            integrity="sha256-fNXJFIlca05BIO2Y5zh1xrShK3ME+/lYZ0j+ChxX2DA="
+            crossorigin="anonymous">
+
+    </script>
 </head>
 <body>
+<div id="brow">
 
+</div>
+<div class="container-fluid" style="margin-top:200px">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <div class="form-horizontal text-center">
+
+
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
-
-
-
-
-
-

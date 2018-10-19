4b22f9a72dff8e6953f9b875cf3e8ad5f999dd0a
Egor <seregin@reffection.com>
2018-10-19 11:01:39 +0300

    Cors

--- resources/views/pixel/index.blade.php
+++ resources/views/pixel/index.blade.php
@@ -3,10 +3,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    {!! $result_op->body !!}
     <title>pixel</title>
 </head>
 <body>
-
+    {!! $result_op->body !!}
 </body>
 </html>

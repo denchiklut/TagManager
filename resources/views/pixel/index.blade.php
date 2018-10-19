f3eccbfd98553302daa525e7d3b0b34f8c4fe0b4
Egor <seregin@reffection.com>
2018-10-19 12:40:52 +0300

    Cors

--- resources/views/pixel/index.blade.php
+++ resources/views/pixel/index.blade.php
@@ -1,1 +1,22 @@
-{!! $result_op->body !!}
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    {!! $result_op->body !!}
+</head>
+<body>
+
+</body>
+</html>
+
+
+
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,3 @@ Route::group(['prefix' => 'pixel','middleware' => 'cors'], function () {
     Route::get('/tags/{hash_advertisings}', 'AdvertisingCampaigns@index');
 
 });
-
-
-Route::get('/pix/', 'AdvertisingPixel@index')->middleware(['cors']);
-
-
-Route::get('pixeltest', ['middleware' => 'cors', function() {
-    return "<script> var script = document.createElement('script'); script.src = '//sonar.semantiqo.com/4e3ll/check.js';  document.getElementsByTagName('head')[0].appendChild(script); </script>";
-}]);

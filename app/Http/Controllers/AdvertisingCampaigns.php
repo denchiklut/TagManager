d939b48e50acbdc35c97c74e43c51646c5c094de
Egor <seregin@reffection.com>
2018-10-20 19:04:15 +0300

    работает

--- app/Http/Controllers/AdvertisingCampaigns.php
+++ app/Http/Controllers/AdvertisingCampaigns.php
@@ -18,24 +18,30 @@ class AdvertisingCampaigns extends Controller
 
 
         $result = Campaigns::where('sig', $hash_advertisings)->first();
+
+
+
         $result_ops = Entities::where('tm_id', $result->tm_id)->get();
 
 
         foreach ($result_ops as $result_op)
         {
-            if ($result_op->entity_id)
-            {
-                $url = Trigers::where('entity_id', $result_op->entity_id)->first();
-                $trigers = $url->url;
+            $url = Trigers::where('entity_id', $result_op->entity_id)->first();
 
-                if (strpos($_SERVER['SERVER_NAME'], $trigers) >=0)
+            if (isset($url->url))
+            {
+                if (strpos($_SERVER['SERVER_NAME'], $url->url) !== false )
                 {
                     return view('pixel.index', compact( 'result_op'));
                 }
             }
-
-            return view('pixel.index', compact( 'result_op'));
-
         }
+
+        $result_op = $result_ops[0];
+
+        return view('pixel.index', compact( 'result_op'));
+
+
+
     }
 }

5161955d507fea9916b9fe4cb4bfbd31e095b8bd
denchiklut <denchiklut@bitbucket.org>
2018-11-16 18:46:19 +0300

    Add Vue Chart

--- resources/js/views/Analytics.vue
+++ resources/js/views/Analytics.vue
@@ -6,7 +6,35 @@
                 <button @click="fillData()">Randomize</button>
                 <line-chart :chart-data="datacollection" :height="120" :options="{responsive: true, maintainAspectRatio: true}" ></line-chart>
             </div>
+            <div>
+                <md-table v-model="people" md-card @md-selected="onSelect">
+                    <md-table-toolbar>
+                        <h1 class="md-title">With auto select and alternate headers</h1>
+                    </md-table-toolbar>
+
+                    <md-table-toolbar slot="md-table-alternate-header" slot-scope="{ count }">
+                        <div class="md-toolbar-section-start">{{ getAlternateLabel(count) }}</div>
+
+                        <div class="md-toolbar-section-end">
+                            <md-button class="md-icon-button">
+                                <md-icon>delete</md-icon>
+                            </md-button>
+                        </div>
+                    </md-table-toolbar>
+
+                    <md-table-row slot="md-table-row" slot-scope="{ item }" :md-disabled="item.name.includes('Stave')" md-selectable="multiple" md-auto-select>
+                        <md-table-cell md-label="Name" md-sort-by="name">{{ item.name }}</md-table-cell>
+                        <md-table-cell md-label="Email" md-sort-by="email">{{ item.email }}</md-table-cell>
+                        <md-table-cell md-label="Gender" md-sort-by="gender">{{ item.gender }}</md-table-cell>
+                        <md-table-cell md-label="Job Title" md-sort-by="title">{{ item.title }}</md-table-cell>
+                    </md-table-row>
+                </md-table>
+
+                <p>Selected:</p>
+                {{ selected }}
+            </div>
         </div>
+        <div class="md-layout-item"> <pre>{{this.data}}</pre></div>
     </div>
 </template>
 
@@ -19,7 +47,40 @@
             datacollection: null,
             options: {
                 responsive: true,
-            }
+            },
+            people: [
+                {
+                    name: 'Shawna Dubbin',
+                    email: 'sdubbin0@geocities.com',
+                    gender: 'Male',
+                    title: 'Assistant Media Planner'
+                },
+                {
+                    name: 'Odette Demageard',
+                    email: 'odemageard1@spotify.com',
+                    gender: 'Female',
+                    title: 'Account Coordinator'
+                },
+                {
+                    name: 'Lonnie Izkovitz',
+                    email: 'lizkovitz3@youtu.be',
+                    gender: 'Female',
+                    title: 'Operator'
+                },
+                {
+                    name: 'Thatcher Stave',
+                    email: 'tstave4@reference.com',
+                    gender: 'Male',
+                    title: 'Software Test Engineer III'
+                },
+                {
+                    name: 'Clarinda Marieton',
+                    email: 'cmarietonh@theatlantic.com',
+                    gender: 'Female',
+                    title: 'Paralegal'
+                }
+            ],
+            selected:[]
         }),
         components: {
             LineChart
@@ -59,6 +120,20 @@
             },
             getRandomInt () {
                 return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+            },
+            onSelect (items) {
+                this.selected = items
+                console.log('select event')
+                console.log(items)
+            },
+            getAlternateLabel (count) {
+                let plural = ''
+
+                if (count > 1) {
+                    plural = 's'
+                }
+
+                return `${count} user${plural} selected`
             }
         },
         created() {
@@ -92,5 +167,9 @@
 
     @import "~vue-material/dist/base/theme";
     @import "~vue-material/dist/components/MdCard/theme";
+
+    .md-table + .md-table {
+        margin-top: 16px
+    }
 
 </style>
